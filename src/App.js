4dad9325d9df6fc33c451d7372f73e923dc289c0
ggustavo-souza <cavalogames1231@gmail.com>
2025-04-23 20:31:03 -0300

    commit fazendo o projeto igual o bro

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import logo from './favicon.png';
+import React from 'react';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import './bootstrap/bootstrap.min.css';
+import ListUser from './components/ListUser';
+import CreateUser from './components/CreateUser';
+import EditUser from './components/EditUser';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav className='App-navbar'>
-            
-        </nav>
-      </header>
+    <div className='App'>
+      <h5>React CRUD usando API and MySQL</h5>
+        <BrowserRouter>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">List Users</Link>
+              </li>
+              <li>
+                <Link to="user/create">Create User</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route index element={<ListUser />} />
+            <Route path='user/create' element={<CreateUser />} />
+            <Route path='user/:id/edit' element={<EditUser />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
